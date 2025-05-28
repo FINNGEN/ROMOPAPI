716312ae8e4d4ee7efa79f97f5766b87bb3da6d1
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2025-05-28 11:13:54 +0300

    test

--- tests/testthat/test-createCodeCountsTable.R
+++ tests/testthat/test-createCodeCountsTable.R
@@ -9,8 +9,15 @@ test_that("createCodeCountsTable works", {
   code_counts <- CDMdbHandler$connectionHandler$tbl("code_counts")  
 
   code_counts |> dplyr::count() |> dplyr::pull(n) |> expect_gt(0)
-  code_counts |> colnames() |> expect_equal(c("domain", "concept_id", "calendar_year", "gender_concept_id", "age_decile", "event_counts",  "person_counts", "incidence_person_counts", "total_person_counts"))
+  code_counts |> colnames() |> expect_equal(c(
+    "domain", "concept_id", 
+    "calendar_year", "gender_concept_id", "age_decile",
+     "event_counts",  "person_counts", "incidence_person_counts",
+      "descendant_event_counts", "descendant_person_counts", "descendant_incidence_person_counts", 
+      "total_person_counts"))
   code_counts |> dplyr::filter(person_counts > event_counts) |> dplyr::count() |> dplyr::pull(n) |> expect_equal(0)
   code_counts |> dplyr::filter(incidence_person_counts > person_counts) |> dplyr::count() |> dplyr::pull(n) |> expect_equal(0)
-
+  code_counts |> dplyr::filter(descendant_person_counts < person_counts) |> dplyr::count() |> dplyr::pull(n) |> expect_equal(0)
+  code_counts |> dplyr::filter(descendant_incidence_person_counts < incidence_person_counts) |> dplyr::count() |> dplyr::pull(n) |> expect_equal(0)
+  code_counts |> dplyr::filter(descendant_event_counts < event_counts) |> dplyr::count() |> dplyr::pull(n) |> expect_equal(0)
 })
