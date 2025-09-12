c9ed47b70cfa77c4e0a38cec1c026e75d0f21d4e
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2025-09-12 13:44:48 +0300

    save

--- tests/testmanual/manualtest-timeplotview.R
+++ tests/testmanual/manualtest-timeplotview.R
@@ -10,7 +10,7 @@ conceptId <- c(317009)  # asthma snomde
 conceptId <- 21601860 # ATC level 5 
 conceptId <- 45596282 # asthma ICD10
 
-conceptId <- 21601855 
+conceptId <- 21601855       # ATC C10AA
 conceptId <- 21601823
 conceptId <- 21601487
 
@@ -42,5 +42,63 @@ results <- pruneLevelsFromResults(results, pruneLevels, pruneClass = "Ingredient
 
 
 # create report
-report.html <- createReport(conceptId, CDMdbHandler, showsMappings = TRUE, pruneLevels = 2)
+# create report
+report.html <- createReport(conceptId, CDMdbHandler, showsMappings = FALSE)
+browseURL(report.html)
+
+report.html <- createReport(conceptId, CDMdbHandler, showsMappings = FALSE, pruneLevels = 2)
+browseURL(report.html)
+
+
+
+#
+# EXAMPLES
+#
+
+# ATC level 4: ATC C10AA
+
+conceptId <- 21601855 
+
+results <- getCodeCounts(
+    CDMdbHandler,
+    conceptId = conceptId
+)
+
+createMermaidGraphFromResults(results) |> clipr::write_clip()
+
+report.html <- createReport(conceptId, CDMdbHandler, showsMappings = FALSE)
+browseURL(report.html)
+
+## Cut to level 2
+results2 <- pruneLevelsFromResults(results, pruneLevels = 2)
+
+createMermaidGraphFromResults(results2) |> clipr::write_clip()
+
+report.html <- createReport(conceptId, CDMdbHandler, showsMappings = FALSE, pruneLevels = 2)
+browseURL(report.html)
+
+## Cut to level 2 and remove ingredients
+results3 <- pruneLevelsFromResults(results, pruneLevels = 2, pruneClass = "Ingredient")
+createMermaidGraphFromResults(results3) |> clipr::write_clip()
+report.html <- createReport(conceptId, CDMdbHandler, showsMappings = FALSE, pruneLevels = 2, pruneClass = "Ingredient")
+browseURL(report.html)
+
+
+## Cut to level 3 
+results4 <- pruneLevelsFromResults(results, pruneLevels = 3)
+createMermaidGraphFromResults(results4) |> clipr::write_clip()
+report.html <- createReport(conceptId, CDMdbHandler, showsMappings = FALSE, pruneLevels = 3)
+browseURL(report.html)
+
+## Cut to level 3 and remove ingredients
+results5 <- pruneLevelsFromResults(results, pruneLevels = 3, pruneClass = "Ingredient")
+createMermaidGraphFromResults(results5) |> clipr::write_clip()
+report.html <- createReport(conceptId, CDMdbHandler, showsMappings = FALSE, pruneLevels = 3, pruneClass = "Ingredient")
+browseURL(report.html)
+
+
+## Cut to level 4 and remove ingredients
+results5 <- pruneLevelsFromResults(results, pruneLevels = 4, pruneClass = "Ingredient")
+createMermaidGraphFromResults(results5) |> clipr::write_clip()
+report.html <- createReport(conceptId, CDMdbHandler, showsMappings = FALSE, pruneLevels = 3, pruneClass = "Ingredient")
 browseURL(report.html)
