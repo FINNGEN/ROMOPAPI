30c1755ad6d27b59c0e9dbd48e41db7aa9226629
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2025-11-10 12:15:24 +0200

    v2.1.0

--- tests/testthat/test-getAPIInfo.R
+++ tests/testthat/test-getAPIInfo.R
@@ -12,13 +12,9 @@ test_that("getAPIInfo works", {
   api_info <- getAPIInfo(CDMdbHandler)
 
   # Check column names 
-  api_info$cdm_source_name |>
-    expect_equal("FinnGen data freeze 13")
-  api_info$cdm_source_abbreviation |>
-    expect_equal("FinnGen-DF13")
-  api_info$vocabulary_version |>
-    expect_equal("v5.0 27-FEB-25")
-  api_info$romop_api_version |>
+  api_info |> names() |>
+    expect_equal(c("cdm_source_name", "cdm_source_abbreviation", "vocabulary_version", "romop_api_version"))
+   api_info$romop_api_version |>
     expect_equal(as.character(utils::packageVersion("ROMOPAPI")))
 
 })
